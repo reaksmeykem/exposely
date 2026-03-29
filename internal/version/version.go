03a6efcefa675193ef58e3fa6e25a230e251f1ac
reaksmeykem <79095458+reaksmeykem@users.noreply.github.com>
2026-03-29 17:37:32 +0700

    Release v1.0.23

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 const (
-	Version        = "1.0.22"
+	Version        = "1.0.23"
 	RepoOwner      = "reaksmeykem"
 	RepoName       = "exposely"
 	ReleasePageURL = "https://github.com/" + RepoOwner + "/" + RepoName + "/releases/latest"
