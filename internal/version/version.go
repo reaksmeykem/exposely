ad3846c9d47314b4b027f7b34cefb106f7642028
reaksmeykem <kemreaksmey444@gmail.com>
2026-05-11 15:46:17 +0700

    Release v1.0.28

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 const (
-	Version        = "1.0.27"
+	Version        = "1.0.28"
 	RepoOwner      = "reaksmeykem"
 	RepoName       = "exposely"
 	ReleasePageURL = "https://github.com/" + RepoOwner + "/" + RepoName + "/releases/latest"
