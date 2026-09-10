63d0878e4f0bb217290fcb178fb7ce08fbb05775
user.email <kemreaksmey444@gmail.com>
2026-09-10 11:56:28 +0700

    Release v1.0.39: recoverable settings store

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 const (
-	Version        = "1.0.38"
+	Version        = "1.0.39"
 	RepoOwner      = "reaksmeykem"
 	RepoName       = "exposely"
 	ReleasePageURL = "https://github.com/" + RepoOwner + "/" + RepoName + "/releases/latest"
