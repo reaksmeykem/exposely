9d347f888a9123bb3f2de9804e10ea7a438e1a02
user.email <kemreaksmey444@gmail.com>
2026-09-10 09:47:42 +0700

    Release v1.0.37: multiple managed PHP versions

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 const (
-	Version        = "1.0.36"
+	Version        = "1.0.37"
 	RepoOwner      = "reaksmeykem"
 	RepoName       = "exposely"
 	ReleasePageURL = "https://github.com/" + RepoOwner + "/" + RepoName + "/releases/latest"
