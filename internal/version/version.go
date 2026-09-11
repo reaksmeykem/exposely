c94bfdf18d9e624caed0dc764628dbc9d8e2616e
user.email <kemreaksmey444@gmail.com>
2026-09-11 08:18:09 +0700

    Release v1.0.40: port 80 serving + named-tunnel managed origin

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 const (
-	Version        = "1.0.39"
+	Version        = "1.0.40"
 	RepoOwner      = "reaksmeykem"
 	RepoName       = "exposely"
 	ReleasePageURL = "https://github.com/" + RepoOwner + "/" + RepoName + "/releases/latest"
